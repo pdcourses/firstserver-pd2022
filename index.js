39623a80c66d6dbe3c8eb560d6db59a9edd87579
pdcourses <freshitpt@gmail.com>
2022-10-16 13:45:03 +0300

    server and app were created

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('./app.js');
+const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || '127.0.0.1';
+
+const server = http.createServer(app);
+
+server.listen(HOST, PORT, 
+    () => console.log('Server is listening...'));
