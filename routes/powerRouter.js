ac75a7d2dbe719bd0c792bf7a824d7bd8e06581c
pdcourses <freshitpt@gmail.com>
2022-10-22 10:57:29 +0300

    power controller: get powers

--- routes/powerRouter.js
+++ routes/powerRouter.js
@@ -2,5 +2,6 @@ const {Router} = require('express');
 const {powerController} = require('../controllers');
 
 const powerRouter = Router();
+powerRouter.get('/', powerController.getPowers);
 
 module.exports = powerRouter;
